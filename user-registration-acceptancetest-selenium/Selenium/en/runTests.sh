c03bbe580e76c5cd62727f24e6e5946af5b94771
Eberhard Wolff <eberhard.wolff@gmail.com>
2016-08-22 21:18:20 +0200

    minor fixes

--- user-registration-acceptancetest-selenium/Selenium/en/runTests.sh
+++ user-registration-acceptancetest-selenium/Selenium/en/runTests.sh
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-2.40.0.jar  -htmlSuite *firefox http://localhost:8080/ RegistrierungTestSuite.html result.html
+java -jar selenium-server-standalone-2.40.0.jar  -htmlSuite *firefox http://localhost:8080/ RegistrationTestSuite.html result.html
